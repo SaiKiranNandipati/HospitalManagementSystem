47b6d5dae487fbcfbced62afdbf0c7009631ef3e
Nandipati <snandip@ilstu.edu>
2023-06-22 13:24:49 -0500

    latest code commit

--- HospitalManagementSystem/src/main/java/com/hms/app/model/Payment.java
+++ HospitalManagementSystem/src/main/java/com/hms/app/model/Payment.java
@@ -28,6 +28,48 @@ public class Payment {
 	private String expiryDate;
 	private String cvv;
 	private String amount;
+	public Long getId() {
+		return Id;
+	}
+	public void setId(Long id) {
+		Id = id;
+	}
+	public String getAppointmentId() {
+		return appointmentId;
+	}
+	public void setAppointmentId(String appointmentId) {
+		this.appointmentId = appointmentId;
+	}
+	public String getCardName() {
+		return cardName;
+	}
+	public void setCardName(String cardName) {
+		this.cardName = cardName;
+	}
+	public String getCardNumber() {
+		return cardNumber;
+	}
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+	public String getExpiryDate() {
+		return expiryDate;
+	}
+	public void setExpiryDate(String expiryDate) {
+		this.expiryDate = expiryDate;
+	}
+	public String getCvv() {
+		return cvv;
+	}
+	public void setCvv(String cvv) {
+		this.cvv = cvv;
+	}
+	public String getAmount() {
+		return amount;
+	}
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
 	
 	
 
